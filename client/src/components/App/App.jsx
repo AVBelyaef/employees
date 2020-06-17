214dd2820431b1e8d468132ad768b4c58c8e06f3
AVBelyaef <avbelyaef@ya.ru>
2020-06-17 20:38:15 +0300

    adding and removing employees

--- client/src/components/App/App.jsx
+++ client/src/components/App/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import EmployesList from '../EmployeesList/EmployesList';
+import NewEmployee from '../NewEmployee/NewEmployee';
+import EditEmployee from '../EditEmployee/EditEmployee';
 
 const App = () => (
   <div>
     <Switch>
-
+      <Route path="/employees/new"><NewEmployee /></Route>
+      <Route path="/employees/:id/edit"><EditEmployee /></Route>
       <Route path="/employees"><EmployesList /></Route>
       <Route path="/" render={() => <Redirect to="/employees" />} />
     </Switch>
--- client/src/redux/saga/saga.js
+++ client/src/redux/saga/saga.js
@@ -2,8 +2,15 @@ import {
   all, put, call, takeEvery,
 } from 'redux-saga/effects';
 
-import { EMPLOYEES_REQUESTED } from '../types';
-import { employeesFailure, employeesSuccess } from '../actions';
+import {
+  EMPLOYEES_REQUESTED, FETCHED_ADD_EMPLOYEE,
+  FETCHED_EMPLOYEE_BY_ID, FETCHED_UPDATE_EMPLOYEE,
+} from '../types';
+
+import {
+  employeesFailure, employeesRequsted, employeesSuccess, employeeRequsted,
+  employeeByIdSuccess,
+} from '../actions';
 
 function* employeesFetchAsync() {
   try {
@@ -19,12 +26,75 @@ function* employeesFetchAsync() {
   }
 }
 
+export function* employeeAddFetchAsync(action) {
+  yield put(employeeRequsted());
+  const {
+    name, phone, birthday, role, isArchive,
+  } = action.payload;
+  try {
+    const data = yield call(() => fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name, phone, birthday, role, isArchive,
+      }),
+    }));
+    if (data.status === 201) {
+      yield put(employeesRequsted());
+    } else {
+      yield put(employeesFailure(data.statusText));
+    }
+  } catch (error) {
+    yield put(employeesFailure(error.message));
+  }
+}
+
+export function* employeeUpdateFetchAsync(action) {
+  yield put(employeeRequsted());
+  try {
+    const {
+      name, phone, birthday, role, isArchive, id,
+    } = action.payload;
+    const result = yield call(() => fetch(`http://localhost:5000/users/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name, isArchive, role, phone, birthday,
+      }),
+    }));
+    if (result.status === 200) {
+      yield put(employeesRequsted());
+    } else {
+      yield put(employeesFailure(result.statusText));
+    }
+  } catch (e) {
+    yield put(employeesFailure(e.message));
+  }
+}
+
+export function* employeeByIdFetchAsync(action) {
+  yield put(employeeRequsted());
+  try {
+    const id = action.payload;
+    const result = yield call(() => fetch(`http://localhost:5000/users/${id}`));
+    const json = yield call(() => result.json());
+    yield put(employeeByIdSuccess(json));
+  } catch (e) {
+    yield put(employeesFailure(e.message));
+  }
+}
+
 function* watchFetches() {
   yield all([
     takeEvery(EMPLOYEES_REQUESTED, employeesFetchAsync),
-    // takeEvery(FETCHED_ADD_EMPLOYEE, employeeAddFetchAsync),
+    takeEvery(FETCHED_ADD_EMPLOYEE, employeeAddFetchAsync),
+    takeEvery(FETCHED_UPDATE_EMPLOYEE, employeeUpdateFetchAsync),
+    takeEvery(FETCHED_EMPLOYEE_BY_ID, employeeByIdFetchAsync),
     // takeEvery(FETCHED_REMOVE_EMPLOYEE, employeeRemoveFetchAsync),
-    // takeEvery(FETCHED_UPDATE_EMPLOYEE, employeeUpdateFetchAsync),
   ]);
 }
 
